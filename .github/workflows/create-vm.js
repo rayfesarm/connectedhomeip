12eb56bded6d09106e8808050a03c88ae2894d01
RonanSynnottArm <68895660+RonanSynnottArm@users.noreply.github.com>
2022-08-10 15:28:22 +0900

    Update create-vm.js

--- .github/workflows/create-vm.js
+++ .github/workflows/create-vm.js
@@ -35,8 +35,8 @@ async function waitForState (instance, callback) {
 }
 
 async function main() {
-  const apiToken = "71819844e83655e4131c.90b2b6b6dc26b791ac7df5c5a9db36651ca10172d24a80a4e13cf217c4a5748e530c73525e9a7a446a91c63a7216dee29520d7ab8a86449cc62ba02b45f0e3c1"
-  
+  const apiToken = process.env.API_TOKEN
+    
   console.log('Logging in...');
   const authInfo = await api.v1AuthLogin({ apiToken });
   BearerAuth.accessToken = authInfo.token
